75f4b7a12240f00f3cb54577fc01d2687b5243c8
EmmanuelMitchell <emmanuelmitchell1998@gmail.com>
2024-05-11 18:44:48 +0000

    setup front and back

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Hello world!
+      </h1>
+      <p>Mern Auth</p>
+    </div>
+  );
+}
